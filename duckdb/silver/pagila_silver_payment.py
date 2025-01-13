a7eab3a4ca8450b5d527e90a574f5fe67e114901
Kalis Rifo Irwandi <80464258+irwandifo@users.noreply.github.com>
2025-01-13 08:03:34 +0700

    Create pagila_silver_payment.py

---
+++ duckdb/silver/pagila_silver_payment.py
@@ -0,0 +1,22 @@
+import duckdb
+from os import getenv
+from fsspec import filesystem
+
+con = duckdb.connect(":memory:")
+con.register_filesystem(filesystem("gcs"))
+
+con.execute(f"""
+  COPY (
+    SELECT
+      payment_id::INTEGER AS payment_id,
+      customer_id::SMALLINT AS customer_id,
+      staff_id::SMALLINT AS staff_id,
+      rental_id::INTEGER AS rental_id,
+      amount::NUMERIC(5, 2) AS amount,
+      payment_date::TIMESTAMPTZ AS paid_at,
+      current_timestamp::TIMESTAMPTZ AS loaded_at
+    FROM read_parquet('{getenv('GCS_URI')}')
+  ) TO 'out.parquet' (FORMAT PARQUET, CODEC SNAPPY)
+""")
+
+con.close()
