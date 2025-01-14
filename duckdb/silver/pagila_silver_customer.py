e715eafb5e16241c059f5ffeeb0dc1e93b8120ef
Kalis Rifo Irwandi <80464258+irwandifo@users.noreply.github.com>
2025-01-14 07:49:02 +0700

    Fix alias

--- duckdb/silver/pagila_silver_customer.py
+++ duckdb/silver/pagila_silver_customer.py
@@ -19,8 +19,8 @@ con.execute(f"""
       ci.city,
       co.country,
       cu.activebool AS is_active,
-      c.create_date::TIMESTAMPTZ AS created_at,
-      c.last_update::TIMESTAMPTZ AS updated_at
+      cu.create_date::TIMESTAMPTZ AS created_at,
+      cu.last_update::TIMESTAMPTZ AS updated_at
     FROM read_parquet('{getenv('GCS_PREFIX')}/customer/*.parquet') cu
     LEFT JOIN read_parquet('{getenv('GCS_PREFIX')}/address/*.parquet') a
       ON c.address_id = a.address_id
