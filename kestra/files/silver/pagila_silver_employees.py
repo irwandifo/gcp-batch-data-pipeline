745abf3765dd41348587c5605066bb2e28881977
Kalis Rifo Irwandi <80464258+irwandifo@users.noreply.github.com>
2025-01-18 18:05:25 +0700

    Create pagila_silver_employees.py

---
+++ kestra/files/silver/pagila_silver_employees.py
@@ -0,0 +1,25 @@
+import duckdb
+from os import getenv
+from fsspec import filesystem
+
+con = duckdb.connect(":memory:")
+con.register_filesystem(filesystem("gcs"))
+
+GCS_PREFIX = getenv("GCS_PREFIX")
+
+con.execute(f"""
+  COPY (
+    SELECT
+      staff_id::INTEGER AS staff_id,
+      store_id::INTEGER AS store_id,
+      address_id::INTEGER AS address_id,
+      concat(s.first_name, ' ', s.last_name) AS name,
+      email,
+      active AS is_active,
+      last_update::TIMESTAMPTZ AS updated_at,
+      loaded_at::TIMESTAMPTZ AS loaded_at
+    FROM read_parquet('{GCS_PREFIX}/staff/*.parquet')
+  ) TO 'out.parquet' (FORMAT PARQUET, CODEC SNAPPY)
+""")
+
+con.close()
