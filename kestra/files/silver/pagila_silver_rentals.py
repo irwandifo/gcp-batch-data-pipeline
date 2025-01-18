01844f429981d2815050004b0009231f84da66d7
Kalis Rifo Irwandi <80464258+irwandifo@users.noreply.github.com>
2025-01-18 17:51:23 +0700

    Create pagila_silver_rentals.py

---
+++ kestra/files/silver/pagila_silver_rentals.py
@@ -0,0 +1,25 @@
+import duckdb
+from os import getenv
+from fsspec import filesystem
+
+con = duckdb.connect(":memory:")
+con.register_filesystem(filesystem("gcs"))
+
+GCS_PREFIX = getenv("GCS_PREFIX")
+
+con.execute(f"""
+  COPY (
+    SELECT
+      rental_id::INTEGER AS rental_id,
+      inventory_id::INTEGER AS inventory_id,
+      customer_id::INTEGER AS customer_id,
+      staff_id::INTEGER AS staff_id,
+      rental_date::TIMESTAMPTZ AS rented_at,
+      return_date::TIMESTAMPTZ AS returned_at,
+      last_update::TIMESTAMPTZ AS updated_at,
+      loaded_at::TIMESTAMPTZ AS loaded_at
+    FROM read_parquet('{GCS_PREFIX}/rental/*/*.parquet')
+  ) TO 'out.parquet' (FORMAT PARQUET, CODEC SNAPPY)
+""")
+
+con.close()
