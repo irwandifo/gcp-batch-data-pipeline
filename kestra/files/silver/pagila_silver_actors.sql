4fdcdca933383f3d5e5edd66039a45053e1dc42c
Kalis Rifo Irwandi <80464258+irwandifo@users.noreply.github.com>
2025-01-21 07:27:08 +0700

    Update pagila_silver_actors.sql

--- kestra/files/silver/pagila_silver_actors.sql
+++ kestra/files/silver/pagila_silver_actors.sql
@@ -1,6 +1,6 @@
 SELECT
   actor_id::INTEGER AS actor_id,
-  concat(first_name, ' ', last_name) AS name,
+  md5(concat(first_name, ' ', last_name)) AS name,
   last_update::TIMESTAMPTZ AS updated_at,
   loaded_at::TIMESTAMPTZ AS loaded_at
 FROM read_parquet('{GCS_PREFIX}/actor/*.parquet')
