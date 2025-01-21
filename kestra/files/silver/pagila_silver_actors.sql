d485249521ed7e460af31c7937a6004c4e2d642f
Kalis Rifo Irwandi <80464258+irwandifo@users.noreply.github.com>
2025-01-21 07:52:36 +0700

    Delete kestra/files/silver/pagila_silver_actors.sql

--- kestra/files/silver/pagila_silver_actors.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-  actor_id::INTEGER AS actor_id,
-  md5(concat(first_name, ' ', last_name)) AS name,
-  last_update::TIMESTAMPTZ AS updated_at,
-  loaded_at::TIMESTAMPTZ AS loaded_at
-FROM read_parquet('{GCS_PREFIX}/actor/*.parquet')
