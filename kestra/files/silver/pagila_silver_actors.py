c45440e83b5d39af0f9fa9bd21a82cd15ffd8d52
Kalis Rifo Irwandi <80464258+irwandifo@users.noreply.github.com>
2025-01-30 08:23:30 +0700

    remove actor name masking

--- kestra/files/silver/pagila_silver_actors.py
+++ kestra/files/silver/pagila_silver_actors.py
@@ -11,7 +11,7 @@ con.execute(f"""
   COPY (
     SELECT
       actor_id::INTEGER AS actor_id,
-      md5(concat(first_name, ' ', last_name)) AS name,
+      concat(first_name, ' ', last_name) AS name,
       last_update::TIMESTAMPTZ AS updated_at,
       loaded_at::TIMESTAMPTZ AS loaded_at
     FROM read_parquet('{GCS_PREFIX}/actor/*.parquet')
