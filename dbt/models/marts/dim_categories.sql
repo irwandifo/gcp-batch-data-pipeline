34c95abe77cf5305e222dfd7c9066953a63bd399
Kalis Rifo Irwandi <80464258+irwandifo@users.noreply.github.com>
2025-02-02 22:12:10 +0700

    Create dim_categories.sql

---
+++ dbt/models/marts/dim_categories.sql
@@ -0,0 +1,14 @@
+with
+
+    stg_categories as (select * from {{ source("pagila_silver", "categories") }}),
+
+    categories as (
+        select
+            stg_categories.category_id as category_sk,
+            stg_categories.category_id as category_nk,
+            stg_categories.name as category_name
+        from stg_categories
+    )
+
+select *
+from categories
