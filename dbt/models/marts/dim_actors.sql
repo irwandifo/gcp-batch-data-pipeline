e1bd8d516984f938e6cdca4e61f48eed1fd00fe4
Kalis Rifo Irwandi <80464258+irwandifo@users.noreply.github.com>
2025-02-02 22:08:29 +0700

    Create dim_actors.sql

---
+++ dbt/models/marts/dim_actors.sql
@@ -0,0 +1,14 @@
+with
+
+    stg_actors as (select * from {{ source("pagila_silver", "actors") }}),
+
+    actors as (
+        select
+            stg_actors.actor_id as actor_sk,
+            stg_actors.actor_id as actor_nk,
+            stg_actors.name as actor_name
+        from stg_actors
+    )
+
+select *
+from actors
