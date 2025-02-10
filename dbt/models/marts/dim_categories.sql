6a5b3e412bf6f78cf210ff06eee8548f097726c6
Kalis Rifo Irwandi <80464258+irwandifo@users.noreply.github.com>
2025-02-10 07:59:30 +0700

    Update dim_categories.sql

--- dbt/models/marts/dim_categories.sql
+++ dbt/models/marts/dim_categories.sql
@@ -4,7 +4,7 @@ with
 
     categories as (
         select
-            stg_categories.category_id as category_sk,
+           {{ dbt_utils.generate_surrogate_key(["stg_categories.category_id"]) }} as category_sk,
             stg_categories.category_id as category_nk,
             stg_categories.name as category_name
         from stg_categories
