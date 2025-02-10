2ba43797ae79b5bd097cd84be5b8966c26c19ba9
Kalis Rifo Irwandi <80464258+irwandifo@users.noreply.github.com>
2025-02-10 07:13:41 +0700

    Create dim_inventories.sql

---
+++ dbt/models/marts/dim_inventories.sql
@@ -0,0 +1,14 @@
+with
+
+    stg_inventories as (select * from {{ source("pagila_silver", "inventories") }}),
+
+    inventories as (
+        select
+            {{ dbt_utils.generate_surrogate_key(["stg_inventories.inventory_id"]) }} as inventory_sk,
+            {{ dbt_utils.generate_surrogate_key(["stg_inventories.film_id"]) }} as film_sk,
+            {{ dbt_utils.generate_surrogate_key(["stg_inventories.store_id"]) }} as store_sk
+        from stg_inventories
+    )
+
+select *
+from inventories
