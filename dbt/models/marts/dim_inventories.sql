7dd9421770ca420de1d739bea7ac6bb5c3637a46
Kalis Rifo Irwandi <80464258+irwandifo@users.noreply.github.com>
2025-02-10 07:16:04 +0700

    Update dim_inventories.sql

--- dbt/models/marts/dim_inventories.sql
+++ dbt/models/marts/dim_inventories.sql
@@ -6,7 +6,8 @@ with
         select
             {{ dbt_utils.generate_surrogate_key(["stg_inventories.inventory_id"]) }} as inventory_sk,
             {{ dbt_utils.generate_surrogate_key(["stg_inventories.film_id"]) }} as film_sk,
-            {{ dbt_utils.generate_surrogate_key(["stg_inventories.store_id"]) }} as store_sk
+            {{ dbt_utils.generate_surrogate_key(["stg_inventories.store_id"]) }} as store_sk,
+            stg_inventories.inventory_id as inventory_nk
         from stg_inventories
     )
 
